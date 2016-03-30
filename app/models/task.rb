167ed3a22140a2e3d6ac92a3d38ca261b39801b5
Steven Miyakawa SAM <samprofessional@gmail.com>
2016-03-30 11:40:38 -0700

    Add ability to edit/add tasks from project

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ActiveRecord::Base
   belongs_to :project
   validates :project_id, presence: true
   validates :status, inclusion: { in: %w(unstarted started finished) }
+  accepts_nested_attributes_for :project
 end
