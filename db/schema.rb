0f293af2664a27cf0b54946a2c8c29fccda712f5
Steven Miyakawa SAM <samprofessional@gmail.com>
2016-03-30 17:13:20 -0700

    Add role column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330180527) do
+ActiveRecord::Schema.define(version: 20160331000953) do
 
   create_table "project_users", force: true do |t|
     t.integer  "project_id"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20160330180527) do
     t.string   "lname"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
